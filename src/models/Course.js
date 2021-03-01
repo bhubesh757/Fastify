20b14c1c004dc4d3ac52b0f0efb90ccbd25c80f1
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-03-01 21:15:59 +0530

    Add files via upload

---
+++ src/models/Course.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const courseSchema = new mongoose.Schema({
+    name : String,
+    price : String,
+    subtitle : String,
+    releaseYear : Number
+})
+
+
+module.exports = mongoose.model('Course' , courseSchema)
